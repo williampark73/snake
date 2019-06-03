ce694a07190e7654c04966caab159364d8c0ceda
James Zhuang <james.zhuang@stanford.edu>
2019-06-03 01:03:52 -0700

    stop human suicide keys

--- human_agent.py
+++ human_agent.py
@@ -6,12 +6,21 @@ def human_agent(game, state):
 	win = state[0]
 	player = state[5]
 
+	prevKey = state[2][player-1]
 	key = state[2][player-1]
 
 	event = win.getch()
 	key = key if event == -1 else event                        # Controls key press
 
 	if key in actions:
+		if key == KEY_RIGHT and prevKey == KEY_LEFT:
+			return prevKey
+		if key == KEY_LEFT and prevKey == KEY_RIGHT:
+			return prevKey
+		if key == KEY_UP and prevKey == KEY_DOWN:
+			return prevKey
+		if key == KEY_DOWN and prevKey == KEY_UP:
+			return prevKey
 		return key
 
 	return state[2][player-1]
