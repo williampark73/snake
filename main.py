37567e357655cb74b2de99462f14b4e6b6d6aa4f
James Zhuang <james.zhuang@stanford.edu>
2019-06-02 17:26:59 -0700

    Temp fix isEnd

--- main.py
+++ main.py
@@ -1,82 +1,32 @@
-"""
-from snake_game import SnakeGame
-from human_agent import human_agent
-
-def play_snake_game(agent_one, agent_two):
-	game = SnakeGame(board_size = (40, 60))
-	print("New Game")
-	state = game.start_state()
-	game.print_board(state)
-
-	while True:
-		action = agent_one(game, state)
-		state = game.successor(state, action)
-		game.print_board(state)
-		if game.is_end(state)[0]:
-			break
-
-		action = agent_two(game, state)
-		state = game.successor(state, action)
-		game.print_board(state)
-		if game.is_end(state)[0]:
-			break
-
-	result = game.is_end(state)[1]
-	if result == 0:
-		print("Tie game")
-	elif result == 1:
-		print("Player 1 wins")
-	else:
-		print("Player 2 wins")
-
-
-
-
-play_snake_game(human_agent, manhattan_agent)
-"""
-
 from snake_game import SnakeGame
 from human_agent import human_agent
 from manhattan_agent import manhattan_agent
 
 def play_snake_game(agent_one, agent_two):
 	game = SnakeGame(board_size = (40, 60))
-	print("New Game")
-
 	state = game.start_state()
-
-	game.print_board(state)	
-
-
+	game.print_board(state)
 
 	while True:
 
 		action = agent_one(game, state)
-		
 		state = game.successor(state, action)
 		game.print_board(state)
-		'''
-		if game.is_end(state)[0]:
+		if game.is_end(state) != -1:
 			break
-		'''
+
 		action = agent_two(game, state)
 		state = game.successor(state, action)
 		game.print_board(state)
-		'''
-		if game.is_end(state)[0]:
+		if game.is_end(state)!= -1:
 			break
-	'''	
-	'''
-	result = game.is_end(state)[1]
+
+	result = game.is_end(state)
 	if result == 0:
 		print("Tie game")
 	elif result == 1:
 		print("Player 1 wins")
 	else:
 		print("Player 2 wins")
-'''
-
-
 
 play_snake_game(human_agent, manhattan_agent)
-
