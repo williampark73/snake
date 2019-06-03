acb381193284760f4230cdded9e301354b938fbd
James Zhuang <james.zhuang@stanford.edu>
2019-06-02 17:06:11 -0700

    Add manhattan agent

---
+++ manhattan_agent.py
@@ -0,0 +1,24 @@
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+
+def manhattan_agent(game, state):
+	actions = game.actions(state)
+
+	turn = state[5]
+	snake = state[1][turn]
+	food = state[4][turn]
+
+	key = None
+
+	if snake[0][0] != food[0]:
+		if snake[0][0] > food[0]:
+			key = KEY_UP
+		else:
+			key = KEY_DOWN
+	else:
+		if snake[0][1] > food[1]:
+			key = KEY_LEFT
+		elif snake[0][1] < food[1]:
+			key = KEY_RIGHT
+		
+	return key
+	
